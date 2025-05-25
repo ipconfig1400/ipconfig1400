9974593689c49ca3fee92f64b6a259322196f24e
IPconfig <50186831+ipconfig1400@users.noreply.github.com>
2025-05-26 01:39:51 +0700

    Update readjson.js

--- src/readjson.js
+++ src/readjson.js
@@ -2,7 +2,7 @@ fetch('./src/data.json')
   .then(response => response.json())
   .then(data => {
     // Use the JSON data here
-    console.log(data);
+    console.log(data["port"]);
   })
   .catch(error => {
     console.error('Error fetching JSON:', error);
