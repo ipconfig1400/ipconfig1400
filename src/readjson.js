4737735e0b54c1f799d91e4be32d5fbd99e5dd2a
IPconfig <50186831+ipconfig1400@users.noreply.github.com>
2025-05-26 01:28:13 +0700

    Create readjson.js

---
+++ src/readjson.js
@@ -0,0 +1,9 @@
+fetch('./src/data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Use the JSON data here
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
