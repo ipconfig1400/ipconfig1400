088e027e6d9f7230bf64b3f0239c942ae33c7eba
IPconfig <50186831+ipconfig1400@users.noreply.github.com>
2025-06-29 10:33:28 +0700

    Delete src directory

--- src/readjson.js
+++
@@ -1,9 +0,0 @@
-fetch('./src/data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Use the JSON data here
-    console.log(data["port"]);
-  })
-  .catch(error => {
-    console.error('Error fetching JSON:', error);
-  });
